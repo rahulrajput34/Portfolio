0df14f7a7196ea35333679f532a5368316cb5c44
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-30 09:54:31 -0400

    fix: give the right navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,8 +48,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Adrian &nbsp;
-            <span className="sm:block hidden"> | JavaScript Mastery</span>
+            Rahul Rajput &nbsp;
+            <span className="sm:block hidden"> | Software Engineer</span>
           </p>
         </Link>
 
