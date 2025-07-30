0576a798f5a9ad07bbb639d1079f0eb63cf2ebc0
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-30 15:42:13 -0400

    feat: add the js and react in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [
       js.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
@@ -22,6 +24,8 @@ export default defineConfig([
         sourceType: "module",
       },
     },
+    plugins: { react },
+    settings: { react: { version: "detect" } },
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
